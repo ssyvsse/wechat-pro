0ded293fa617cac0cc8d45a3743093e705834b35
linliangbin <515124984@qq.com>
2017-12-09 18:01:57 +0800

    sdfsdf

--- spring-boot-project/src/main/java/com/ssyvsse/wechat/menu/MenuManage.java
+++ spring-boot-project/src/main/java/com/ssyvsse/wechat/menu/MenuManage.java
@@ -27,7 +27,7 @@ public class MenuManage {
 		ViewButton mainBtn3 = new ViewButton();
 		mainBtn3.setName("百度");
 		mainBtn3.setType("view");
-		mainBtn3.setUrl(WechatPropUtil.btn22Url.replace("APPID",WechatPropUtil.app_id).replace("REDIRECT_URI", URLEncoder.encode("http://www.baidu.com/","UTF-8")));
+		mainBtn3.setUrl(WechatPropUtil.btn22Url.replace("APPID",WechatPropUtil.app_id).replace("REDIRECT_URI", URLEncoder.encode("http://ssyvsse.s1.natapp.cc/baidu","UTF-8")));
 		/**
 		 * 这是公众号xiaoqrobot目前的菜单结构，每个一级菜单都有二级菜单项 在某个一级菜单下没有二级菜单的情况，menu该如何定义呢？
 		 * 比如，第三个一级菜单项不是“更多体验”，而直接是“幽默笑话”，那么menu应该这样定义： menu.setButton(new Button[] {
